d8a6294ac27cdf5c8d4cb7078756c1f1de980b6e
Barry Ivan Silber <age.ef.ess@gmail.com>
2019-05-29 20:18:15 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,26 @@
 def badge_maker(name)
   returns "Hello, my name is #{name}."
 end
+
+
+
+def batch_badge_creator(names)
+names.map do |name|
+badge_maker(name)
+end
+end
+
+def assign_rooms(speakers)
+speakers.map.with_index do |name, index|
+"Hello, #{name}! You'll be assigned to room #{index + 1}!"
+end
+end
+
+def printer(attendees)
+batch_badge_creator(attendees).each do |person|
+puts person
+end
+assign_rooms(attendees).each do |person|
+puts person
+end
+end
